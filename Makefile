pojntfx/slstatus

commit 04a4bb492162ac7384d3c987581d0894405f1f48

Fix missing config.h in Makefile

Makefile
@@ -5,7 +5,7 @@
 include config.mk
 
 REQ = util
-HDR = arg.h
+HDR = arg.h config.h
 COM =\
 	battery\
 	cpu\
@@ -51,6 +51,9 @@ uptime.o: uptime.c config.mk $(HDR) $(REQ:=.h)
 user.o: user.c config.mk $(HDR) $(REQ:=.h)
 volume.o: volume.c config.mk $(HDR) $(REQ:=.h)
 wifi.o: wifi.c config.mk $(HDR) $(REQ:=.h)
+
+config.h:
+	cp config.def.h $@
 
 .o:
 	$(CC) -o $@ $(LDFLAGS) $< $(COM:=.o) $(REQ:=.o) $(LDLIBS)

commit 109eaf52ed4a99d83b5091472b008b10abae83a8

Get rid of HDR variable
The HDR variable is used for general purpose headers, which are not
given in this project. arg.h and config.h are only needed for
slstatus.c, not every component $(COM:=.c).

This also fixes a problem introduced in
04a4bb492162ac7384d3c987581d0894405f1f48, where config.h is pulled into
the tarball.

Makefile
@@ -5,7 +5,6 @@
 include config.mk
 
 REQ = util
-HDR = arg.h config.h
 COM =\
 	battery\
 	cpu\
@@ -30,8 +29,8 @@ COM =\
 all: slstatus
 
 slstatus: slstatus.o $(COM:=.o) $(REQ:=.o)
-slstatus.o: slstatus.c slstatus.h $(HDR) $(REQ:=.h)
-$(COM:=.o): config.mk $(HDR) $(REQ:=.h)
+slstatus.o: slstatus.c slstatus.h arg.h config.h $(REQ:=.h)
+$(COM:=.o): config.mk $(REQ:=.h)
 
 config.h:
 	cp config.def.h $@
@@ -49,7 +48,7 @@ dist:
 	rm -rf "slstatus-$(VERSION)"
 	mkdir -p "slstatus-$(VERSION)"
 	cp -R LICENSE Makefile README config.mk config.def.h \
-	      $(HDR) slstatus.c $(COM:=.c) $(REQ:=.c) $(REQ:=.h) \
+	      arg.h slstatus.c $(COM:=.c) $(REQ:=.c) $(REQ:=.h) \
 	      slstatus.1 "slstatus-$(VERSION)"
 	tar -cf - "slstatus-$(VERSION)" | gzip -c > "slstatus-$(VERSION).tar.gz"
 	rm -rf "slstatus-$(VERSION)"